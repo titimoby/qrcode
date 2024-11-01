b2ed8716e382600f21061fbe7792b9412b569640
Thierry Chantier <titimoby@gmail.com>
2024-11-01 18:23:59 +0100

    add round elements in qr code & generate on enter key press

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
-import qrcode as qr
+import qrcode
+from qrcode.image.styledpil import StyledPilImage
+from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
 import io
 
 st.title("QR code generation")
@@ -8,8 +10,12 @@ text_input = st.text_input("What text do you want to encode?")
 action_button = st.button("Generate QR code")
 image_element = st.image("./Calm.jpg")
 
-if action_button:
-    image_pil = qr.make(text_input)
+if action_button or text_input:
+    qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+    qr.add_data(text_input)
+    image_pil = qr.make_image(
+        image_factory=StyledPilImage, module_drawer=RoundedModuleDrawer()
+    )
     buffer = io.BytesIO()
     image_pil.save(buffer, format="PNG")
     image_element.image(buffer.getvalue())
